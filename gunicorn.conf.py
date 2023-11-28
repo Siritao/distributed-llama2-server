5590eb3c111246ede705dcde140f5968460e25d6
Siritao <siruitao@stu.pku.edu.cn>
2023-11-28 17:02:14 +0800

    fix bug and add explanation in gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,8 +5,14 @@ open('logs/error.log', 'a').close()
 open('logs/access.log', 'a').close()
 
 bind = '0.0.0.0:5000'
-worker = 1
+workers = 1
+threads = 4
 timeout = 600
 errorlog = 'logs/error.log'
 accesslog = 'logs/access.log'
 loglevel = 'info'
+
+# Note: We cannot use multiple workers together with preload_app in gunicorn to
+# boost memory and compute efficiency, for bad compatibility with CUDA.
+# set workers -> multi-process
+# set threads -> multi-thread
